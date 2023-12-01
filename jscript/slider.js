53807d04b069d772a00b03c6627bbcd669c2ca0b
frontMan <mr.cdrmn277@gmail.com>
2023-12-01 16:02:03 +0300

    added slider (swiper library)

---
+++ jscript/slider.js
@@ -0,0 +1,23 @@
+new Swiper(".swiper", {
+    spaceBetween: 30,
+    loop: true,
+    direction: 'horizontal',
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        425: {
+            slidesPerView: 2,
+        },
+        767: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+    lazy: {
+        loadPrevNext: false,
+        loadOnTransitionStart: false,
+    }
+})
