20598b784618f7d806d7ee82db1508f94d4bfde8
frontMan <mr.cdrmn277@gmail.com>
2023-12-03 18:05:44 +0300

    fixed any problems from (1023 > 768), (767 > 425) and (424 > 300)px resolutions. Images optimized.

--- jscript/header.js
+++ jscript/header.js
@@ -1,17 +1,13 @@
 $(window).scroll(function () {
     var secondBox = $('.second');
     var offset = secondBox.offset().top;
-
-    // Проверяем ширину экрана
     if ($(window).width() > 1023) {
-        // Продолжаем с вашими текущими условиями
         if ($(window).scrollTop() + $(window).height() > offset) {
             $("#callback").slideUp(500);
         } else {
             $("#callback").slideDown(400);
         }
     } else {
-        // Если ширина экрана менее 1023px, скрываем #callback без анимации
         $("#callback").hide();
     }
 })
